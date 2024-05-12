ed632b3f82da9379fe27641e27a2b06fcfd9edcc
Marco Monaco <m.monaco@reply.it>
2024-05-12 17:19:50 +0200

    Add more data

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 const port = 8000;
 
-const fs = require("fs");
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+
+const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+        max: 8,
+        min: 4,
+    },
+    wordsPerSentence: {
+        max: 16,
+        min: 4,
+    },
+});
 
 const MOVIE_DATA = JSON.parse(fs.readFileSync("./src/data/movie_data.json"))[
     "data"
@@ -12,6 +25,12 @@ const HOMEPAGE_DATA = JSON.parse(fs.readFileSync("./src/data/homepage.json"))[
     "getHomepages"
 ];
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 //const csvToJson = require("convert-csv-to-json");
 
 // const moviesData = csvToJson
@@ -19,15 +38,41 @@ const HOMEPAGE_DATA = JSON.parse(fs.readFileSync("./src/data/homepage.json"))[
 //     .getJsonFromCsv("src/data/mubi_movie_data.csv");
 
 //
-// fs.writeFile(
-//     "./src/data/movie_data.json",
-//     JSON.stringify(data),
-//     function (err) {
-//         if (err) {
-//             console.log(err);
+
+// MOVIE_DATA.forEach((element) => {
+//     element.movie_description = lorem.generateSentences(getRandomInt(3, 6));
+// });
+
+// const x = { data: MOVIE_DATA };
+
+// fs.writeFile("./src/data/movie_data.json", JSON.stringify(x), function (err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
+
+// HOMEPAGE_DATA.forEach((homepage) => {
+//     homepage.sections = [];
+//     for (let y = 0; y < 20; y++) {
+//         homepage.sections[y] = {
+//             title: lorem.generateWords(getRandomInt(2, 5)),
+//             type: ["PosterCard", "VideoCard"][getRandomInt(0, 1)],
+//             data: [],
+//         };
+
+//         for (let x = 0; x < getRandomInt(6, 12); x++) {
+//             homepage.sections[y].data.push(MOVIE_DATA[getRandomInt(0, 260000)]);
 //         }
 //     }
-// );
+// });
+
+// const y = { getHomepages: HOMEPAGE_DATA };
+
+// fs.writeFile("./src/data/homepage.json", JSON.stringify(y), function (err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
